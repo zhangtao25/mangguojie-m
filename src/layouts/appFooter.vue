c067f8066237ba534d88c4979507484477213c82
zhangtao25 <zhangtao25@lenovo.com>
2019-07-17 00:09:36 +0800

    .

---
+++ src/layouts/appFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app-footer">
+    <ul>
+      <li v-for="(item,index) of arr" @click="fn(item.path)" :key="index">{{index}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+      return{
+        arr:[
+          {
+            path:'/appHome'
+          },
+          {
+            path:'/mall'
+          },
+        ]
+      }
+    },
+    methods:{
+      fn(path){
+        this.$router.push({path:path})
+      }
+    }
+  }
+</script>
+<style>
+  #app-footer{
+    width: 375px;
+    height: 50px;
+    background-color: #0c82ff;
+  }
+</style>
