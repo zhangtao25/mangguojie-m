b258eae9de335a944294a349fdf364fb2dfaaf24
Allen Zhang <tzhangm@ctrip.com>
2019-07-17 19:46:21 +0800

    .

--- src/layouts/login.vue
+++ src/layouts/login.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
-    我是登陆
-    <a-icon type="alibaba"></a-icon>
+    <div>
+      <a-icon type="left"></a-icon>
+      <span>验证码登录</span>
+    </div>
+
+    <van-cell-group>
+      <van-field
+        v-model="username"
+        label="用户名"
+        placeholder="请输入用户名"
+        error
+      />
+      <van-field
+        v-model="phone"
+        label="手机号"
+        placeholder="请输入手机号"
+        error-message="手机号格式错误"
+      />
+    </van-cell-group>
   </div>
 </template>
 <script>
@@ -11,8 +28,24 @@
       LoginService.login().then(res=>{
         console.log(res)
       })
-
       console.log(window.navigator)
+    },
+    methods:{
+      onLoginClick(){
+
+      }
     }
   }
 </script>
+<style>
+  .m_input{
+    border: none;
+    border-bottom: 1px solid black;
+    caret-color: red;
+    font-weight: bolder;
+    font-size: 18px;
+  }
+  .btn{
+    border: none;
+  }
+</style>
